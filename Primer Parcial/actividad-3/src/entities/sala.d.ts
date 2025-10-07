44339051a7e089cd6371bcdc25cc5f3fba11a065
marco <marcoadrianzambra@gmail.com>
2025-10-07 18:21:35 -0500

    Add entity classes for Cliente, Funcion, Pelicula, and Sala; configure AppDataSource for DataBase

---
+++ Primer Parcial/actividad-3/src/entities/sala.d.ts
@@ -0,0 +1,8 @@
+export declare class Sala {
+    id: string;
+    nombre: string;
+    capacidad: number;
+    tipo: string;
+    estado: "disponible" | "ocupada" | "mantenimiento";
+}
+//# sourceMappingURL=sala.d.ts.map
