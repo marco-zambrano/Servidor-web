508c60eb06e32e4a9dbb68bc4c03608829974671
marco <marcoadrianzambra@gmail.com>
2025-10-23 09:15:29 -0500

    delete: previous graphql project

--- Primer Parcial/taller-5/graphql-gateway/src/reportes/entities/reporte.entity.ts
+++
@@ -1,7 +0,0 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
-
-@ObjectType()
-export class Reporte {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
-}
