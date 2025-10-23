6758febc52afb6f4e22c3c6d6a74618483bba38a
marco <marcoadrianzambra@gmail.com>
2025-10-22 22:46:32 -0500

    add: creation of services that consume the API REST, using http service

--- Primer Parcial/taller-5/graphql-gateway/src/app.module.ts
+++ Primer Parcial/taller-5/graphql-gateway/src/app.module.ts
@@ -3,6 +3,15 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { HttpModule } from '@nestjs/axios';
 import { join } from 'path';
+import { UsuariosRestService } from './usuarios/usuarios-rest/usuarios-rest.service';
+import { SalasRestService } from './salas/salas-rest/salas-rest.service';
+import { AsientosRestService } from './asientos/asientos-rest/asientos-rest.service';
+import { PeliculasRestService } from './peliculas/peliculas-rest/peliculas-rest.service';
+import { FuncionesRestService } from './funciones/funciones-rest/funciones-rest.service';
+import { ReservasRestService } from './reservas/reservas-rest/reservas-rest.service';
+import { ReservasAsientosRestService } from './reservas-asientos/reservas-asientos-rest/reservas-asientos-rest.service';
+import { FacturasRestService } from './facturas/facturas-rest/facturas-rest.service';
+import { ReportesRestService } from './reportes/reportes-rest/reportes-rest.service';
 
 @Module({
   imports: [
@@ -19,5 +28,6 @@ import { join } from 'path';
     }),
     // Aquí se importarán los módulos de resolvers
   ],
+  providers: [UsuariosRestService, SalasRestService, AsientosRestService, PeliculasRestService, FuncionesRestService, ReservasRestService, ReservasAsientosRestService, FacturasRestService, ReportesRestService],
 })
 export class AppModule {}
