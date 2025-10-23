3e9c9752827274e170ea27162743394bd8f58c55
marco <marcoadrianzambra@gmail.com>
2025-10-23 10:22:17 -0500

    @as-integrations/express5 dependecy installed

--- Primer Parcial/taller-5/graphql-gateway/src/peliculas/peliculas.module.ts
+++ Primer Parcial/taller-5/graphql-gateway/src/peliculas/peliculas.module.ts
@@ -1,11 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { PeliculasService } from './peliculas.service';
 import { PeliculasResolver } from './peliculas.resolver';
 import { FuncionModule } from '../funcion/funcion.module';
 
 @Module({
-  imports: [HttpModule, FuncionModule],
+  imports: [HttpModule, forwardRef(() => FuncionModule)],
   providers: [PeliculasResolver, PeliculasService],
   exports: [PeliculasService],
 })
