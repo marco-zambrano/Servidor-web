772e7bb1a3ec12736354a0966bca4d8552fd2933
marco <marcoadrianzambra@gmail.com>
2025-10-22 22:10:56 -0500

    add: graphql module basic configuration

--- Primer Parcial/taller-5/graphql-gateway/src/app.module.ts
+++ Primer Parcial/taller-5/graphql-gateway/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { HttpModule } from '@nestjs/axios';
+import { join } from 'path';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+      playground: true, // Apollo Playground
+    }),
+    HttpModule.register({
+      baseURL: 'http://localhost:3000', // URL del servicio REST
+      timeout: 5000,
+      maxRedirects: 5,
+    }),
+    // Aquí se importarán los módulos de resolvers
+  ],
 })
 export class AppModule {}
