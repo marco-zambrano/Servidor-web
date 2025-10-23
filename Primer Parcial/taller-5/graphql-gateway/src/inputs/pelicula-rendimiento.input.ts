37d8755dfa96ea1f53d8dd1509709271452ba635
marco <marcoadrianzambra@gmail.com>
2025-10-22 22:42:17 -0500

    add: integrante 1 - consulta de informacion agregada, input types

---
+++ Primer Parcial/taller-5/graphql-gateway/src/inputs/pelicula-rendimiento.input.ts
@@ -0,0 +1,58 @@
+import { ObjectType, Field, ID, Float, Int } from '@nestjs/graphql';
+
+@ObjectType()
+export class PeliculaRendimientoType {
+  @Field(() => ID)
+  id_pelicula: string;
+
+  @Field()
+  titulo: string;
+
+  @Field()
+  genero: string;
+
+  @Field({ nullable: true })
+  descripcion?: string;
+
+  @Field({ nullable: true })
+  clasificacion?: string;
+
+  @Field(() => Int)
+  total_funciones: number;
+
+  @Field(() => Int)
+  funciones_activas: number;
+
+  @Field(() => Int)
+  total_reservas: number;
+
+  @Field(() => Float)
+  ingreso_total: number;
+
+  @Field(() => Float)
+  promedio_ocupacion: number;
+
+  @Field(() => [FuncionResumenType])
+  funciones: FuncionResumenType[];
+}
+
+@ObjectType()
+export class FuncionResumenType {
+  @Field(() => ID)
+  id_funcion: string;
+
+  @Field()
+  fecha_hora: Date;
+
+  @Field()
+  nombre_sala: string;
+
+  @Field(() => Float)
+  precio: number;
+
+  @Field(() => Int)
+  reservas: number;
+
+  @Field(() => Float)
+  ocupacion: number;
+}
